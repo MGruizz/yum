53a0cacbbbcbf2b16b1a6e8d2af6731027dd35e1
Miguel Guerrero <mguerrero@subcargo.com>
2023-04-15 21:02:04 -0400

    se crea pagina resultado busqueda, lista recetas

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/LoginPage/LoginPage';
 import FrontPage from './pages/FrontPage/FrontPage';
 import UserProfile from './pages/UserProfile/UserProfile';
+import PostList from './pages/PostList/PostList';
 
 const AppRouter: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const AppRouter: React.FC = () => {
         <Route path="/" element={<FrontPage/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/profile" element={<UserProfile/>} />
+        <Route path="/search" element={<PostList/>} />
         {/* Agrega otras rutas aquí según sea necesario */}
       </Routes>
     </Router>
