b329c51848539252b613130d09992f78ea145094
Miguel Guerrero <mguerrero@subcargo.com>
2023-04-21 18:07:19 -0400

    se arregla responsividad banner seccion

--- src/components/BannerSeccion/BannerSeccion.tsx
+++ src/components/BannerSeccion/BannerSeccion.tsx
@@ -9,13 +9,13 @@ const bannerSeccion = [
 const BannerSeccion = () => {
 
     return (
-        <div className="flex items-center bg-white">
-            <img className="flex-shrink-0" src="https://via.placeholder.com/720x300" alt="banner-seccion" />
-            <div className="text-center px-5">
-                <p className="mb-4">Explora nuestras secciones y descubre toda una gama de comidas que te ayuden a sentirte bien y mantener un estilo de vida activo.</p>
-                <p>Si estás buscando comenzar tu camino en el mundo culinario, estás en el lugar indicado. En YUM tienes acceso a toda una librería de recetas en la que podrás descubrir nuevos colores y sabores, mientras interactúas con nuestra creciente comunidad.</p>
-            </div>
-        </div>
+        <div className="flex flex-col md:flex-row items-center bg-white">
+  <img className="flex-shrink-0 mb-4 md:mb-0 md:mr-4" src="https://via.placeholder.com/720x300" alt="banner-seccion" />
+  <div className="text-center">
+    <p className="mb-4">Explora nuestras secciones y descubre toda una gama de comidas que te ayuden a sentirte bien y mantener un estilo de vida activo.</p>
+    <p>Si estás buscando comenzar tu camino en el mundo culinario, estás en el lugar indicado. En YUM tienes acceso a toda una librería de recetas en la que podrás descubrir nuevos colores y sabores, mientras interactúas con nuestra creciente comunidad.</p>
+  </div>
+</div>
     )
 }
 
