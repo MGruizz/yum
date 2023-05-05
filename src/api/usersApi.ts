f08f99cb07ffb7b5f147e6f658ae3cc2311e1686
Miguel Guerrero <mguerrero@subcargo.com>
2023-05-04 20:13:23 -0400

    merge con ramas

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -1,14 +1,15 @@
-import instance from "./axiosInstance";
+import axios from "./axiosInstance";
 import { User , LoginFormValues} from "../features/user/userInterfaces";
 export const registerUser = async (email: string, password: string) => {
   try {
-    const response = await instance.post("/auth/register", {
+    const response = await axios.post("/auth/register", {
       email,
       password,
     });
     return response.data;
   } catch (error) {
-    throw error;
+    console.log('Error',error);
+    
   }
 };
 
@@ -17,15 +18,14 @@ export const registerUser = async (email: string, password: string) => {
 
 export const loginUser = async (values: LoginFormValues) => {
   try {
-    const response = await instance.post("/login/", {
+    const response = await axios.post("/login/", {
       email: values.email,
       password: values.password,
     });
     console.log(response.data, "Funciona ");
     return response.data;
   } catch (error) {
-    console.log("No funciona, osea si funciona ")
-    throw error;
+    console.log('Error',error);
   }
 };
 
@@ -35,7 +35,7 @@ export const loginUser = async (values: LoginFormValues) => {
 
 export const getUserById = async (id: number): Promise<User> => {
   try {
-    const response = await instance.get<User>(`/usuarios/${id}`);
+    const response = await axios.get<User>(`/usuarios/${id}`);
     return response.data;
   } catch (error) {
     throw new Error('Error al obtener información del usuario');
