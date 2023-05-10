01a84acedb1b4d63d6ccab67e0bc6aefbfa68f6d
Miguel Guerrero <mguerrero@subcargo.com>
2023-05-09 23:28:23 -0400

    se agrega select

--- src/components/Categories/Categorias.tsx
+++ src/components/Categories/Categorias.tsx
@@ -1,68 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { getCategories } from "../../api/recipeApi";
 import { Categories } from "../../interfaces/Categories/Categories";
-import Select from "react-select";
-import { OptionTypeBase, SingleValue } from 'react-select';
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-const Categorias: React.FC<Categories> = () => {
-    const [categorias, setCategorias] = useState<Categories[]>([]);
+const Categorias: React.FC = () => {
+  const [categorias, setCategorias] = useState<Categories[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const categoriasData = await getCategories();
-            setCategorias(categoriasData);
-        };
-        fetchData();
-    }, []);
-
-    const [selectedCategories, setSelectedCategories] = useState<Categories[]>([]);
-
-    const handleSelect = (selectedOption: SingleValue<OptionTypeBase>) => {
-        if (selectedOption) {
-            const { value } = selectedOption as { value: number; label: string };
-            const category = categorias.find((cat) => cat.id === value);
-            if (category) {
-                setSelectedCategories([...selectedCategories, category]);
-                setCategorias(categorias.filter((cat) => cat.id !== category.id));
-            }
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      const categoriasData = await getCategories();
+      setCategorias(categoriasData);
     };
+    fetchData();
+  }, []);
 
-    const handleRemove = (index: number) => {
-        const removedCategory = selectedCategories[index];
-        setSelectedCategories(selectedCategories.filter((_, i) => i !== index));
-        setCategorias([...categorias, removedCategory]);
-    };
+  const [selectedCategories, setSelectedCategories] = useState<Categories[]>([]);
 
-    const options = categorias.map((category) => ({
-        value: category.id,
-        label: category.nombre,
-    }));
+  const handleSelect = (category: Categories | null) => {
+    if (category) {
+      setSelectedCategories([...selectedCategories, category]);
+      setCategorias(categorias.filter((cat) => cat.id !== category.id));
+    }
+  };
 
-    return (
-        <div>
-            <Select
-                options={options}
-                onChange={handleSelect}
-                placeholder="Selecciona una categoría"
-                isClearable
-                isSearchable
-            />
-            <div className="mt-4 border border-gray-200 p-2 max-h-20 overflow-auto">
-                {selectedCategories.map((category, index) => (
-                    <div key={index} className="flex items-center justify-between py-1">
-                        <span>{category.nombre}</span>
-                        <button
-                            onClick={() => handleRemove(index)}
-                            className="bg-red-500 text-white px-2 py-1 text-xs rounded-md focus:outline-none"
-                        >
-                            -
-                        </button>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  const handleRemove = (index: number) => {
+    const removedCategory = selectedCategories[index];
+    setSelectedCategories(selectedCategories.filter((_, i) => i !== index));
+    setCategorias([...categorias, removedCategory]);
+  };
+
+  return (
+    <div>
+      <Autocomplete
+        options={categorias}
+        getOptionLabel={(option) => option.nombre}
+        onChange={(_, value) => handleSelect(value)}
+        renderInput={(params) => (
+          <TextField {...params} label="Selecciona una categoría" variant="outlined" />
+        )}
+        style={{ marginBottom: "1rem" }}
+      />
+      <div className="mt-4 border border-gray-200 p-2 max-h-20 overflow-auto">
+        {selectedCategories.map((category, index) => (
+          <div key={index} className="flex items-center justify-between py-1">
+            <span>{category.nombre}</span>
+            <IconButton
+              onClick={() => handleRemove(index)}
+              className="bg-red-500 text-white px-2 py-1 text-xs rounded-md focus:outline-none"
+            >
+              <DeleteIcon />
+            </IconButton>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Categorias;
