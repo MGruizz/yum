23d576f73c56db7b351fcd9f6d6e64f683550db4
Camila Escobedo <camila.escobedo.f@mail.pucv.cl>
2023-06-01 21:53:05 -0400

    feat(Perfil): publicaciones, seguidores, seguidos

--- src/features/user/userInterfaces.ts
+++ src/features/user/userInterfaces.ts
@@ -1,14 +1,20 @@
 export interface User {
-    id: number;
-    username:  string;
-    email: string;
-    descripcion: string;
-    foto_perfil: string;
-    is_admin?: Boolean;
-  }
+  id: number;
+  username: string;
+  email: string;
+  descripcion: string;
+  foto_perfil: string;
+  is_admin?: Boolean;
+}
 
 export interface LoginFormValues {
-    email: string;
-    password: string;
-  }
+  email: string;
+  password: string;
+}
+
+export interface UserInformation {
+  seguidores: number,
+  seguidos: number,
+  publicaciones: number
+}
 
